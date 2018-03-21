8ea037ec8ee146ea4d47246a6db124d13b2742fa
Sateesh Mucharla <36142136+smucharla@users.noreply.github.com>
2018-03-21 07:11:20 -0700

    updated
    revisited the list sequence.

--- lists.py
+++ lists.py
@@ -1,10 +1,31 @@
-# area variables (in square meters)
-hall = 11.25
-kit = 18.0
-liv = 20.0
-bed = 10.75
-bath = 9.50
 
-# house information as list of lists
-house = [["hallway", hall],
-         ["kitchen", kit],
+my_list = [0, 1, 2, 3, 4, 5]
+print(my_list)
+print(my_list[3])
+print(my_list[-1])
+print(my_list[3:])
+print(my_list[:2])
+print(my_list[0::1])
+print(my_list[0:-1:1])
+print(my_list[0::2])
+print(my_list[::2])
+my_list[-1]="a"
+print(my_list)
+my_list[-1]=5
+my_list.append(6)
+print(my_list)
+my_list += [7, 8, 9]
+print(my_list)
+my_list[1:3] = ['b', 'c']
+print(my_list)
+my_list[0] = 'a'
+my_list[3:] = []
+print(my_list)
+my_list[-1:] = ['d', 'a']
+print(my_list)
+my_list.remove('a')
+print(my_list)
+print(my_list.pop())
+print(my_list.pop(-1))
+print(my_list)
+

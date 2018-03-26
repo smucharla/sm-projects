64942a12d459c27a82f75c16007444030d331d11
Sateesh Mucharla <36142136+smucharla@users.noreply.github.com>
2018-03-25 20:36:59 -0700

    New BMI Calculator
    Program to calculate BMI

--- lists.py
+++ lists.py
@@ -1,31 +1,15 @@
 
-my_list = [0, 1, 2, 3, 4, 5]
-print(my_list)
-print(my_list[3])
-print(my_list[-1])
-print(my_list[3:])
-print(my_list[:2])
-print(my_list[0::1])
-print(my_list[0:-1:1])
-print(my_list[0::2])
-print(my_list[::2])
-my_list[-1]="a"
-print(my_list)
-my_list[-1]=5
-my_list.append(6)
-print(my_list)
-my_list += [7, 8, 9]
-print(my_list)
-my_list[1:3] = ['b', 'c']
-print(my_list)
-my_list[0] = 'a'
-my_list[3:] = []
-print(my_list)
-my_list[-1:] = ['d', 'a']
-print(my_list)
-my_list.remove('a')
-print(my_list)
-print(my_list.pop())
-print(my_list.pop(-1))
-print(my_list)
 
+my_list = [1 ,2 ,3 ,4, 5]
+my_tuple = (1, 2, 3, 4, 5)
+my_dict = {1:1, 2:2, 3:3, 4:4, 5:5}
+print (my_list)
+print(my_tuple)
+print(my_dict)
+my_list[0] = 0
+print(my_list[0])
+print(my_tuple[0])
+#my_tuple[0] = 0
+sateesh = ['a']
+sateesh[0] = 's'
+print(sateesh)
